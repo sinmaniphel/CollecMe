8cb4a0af402a7af8a9c49c9cacdcf5e36be2e732
aggelos <sinmaniphel@free.fr>
2015-05-16 22:33:22 +0200

    translator update

--- src/CollecMe/CollectionBundle/Controller/DefaultController.php
+++ src/CollecMe/CollectionBundle/Controller/DefaultController.php
@@ -8,10 +8,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-    * @Route("/hello/{name}")
+    * @Route("/")
     */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('CollecMeCollectionBundle:Display:item.html.twig', array('name' => $name));
+        return $this->render('CollecMeCollectionBundle:Display:item.html.twig');
     }
 }
