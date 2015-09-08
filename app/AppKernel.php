5316e1c3311c6ae2f013ad90eaea04f82fe29957
aggelos <sinmaniphel@free.fr>
2015-09-08 11:20:24 +0200

    Alright, first commit for the item list

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
             new JMS\TranslationBundle\JMSTranslationBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
             new CollecMe\CollectionBundle\CollecMeCollectionBundle()
         );
 
